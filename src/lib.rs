68f2aa4d782b88ed48541b44c418bcb09c682dcd
Sturdy <sturdy@apollo.farm>
2022-11-26 02:03:24 +0100

    feat: Make cargo feature for astroport

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 pub mod app;
 pub mod application;
-pub mod astroport;
 pub mod chain;
 pub mod config;
+
+#[cfg(feature = "astroport")]
+#[cfg_attr(docsrs, doc(cfg(feature = "astroport")))]
+pub mod astroport;
 
 pub use testcontainers::clients::Cli;
