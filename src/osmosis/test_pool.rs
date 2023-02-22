4b1d61e1887c3e8a8a66ecafb3c1cff73dab98f6
Sturdy <sturdy@apollo.farm>
2023-02-22 17:21:52 +0100

    feat: Add more proptest generator functions

--- src/osmosis/test_pool.rs
+++ src/osmosis/test_pool.rs
@@ -1,4 +1,6 @@
-use cosmwasm_std::Coin;
+use std::ops::Range;
+
+use cosmwasm_std::{Coin, Uint128};
 use osmosis_std::types::osmosis::gamm::poolmodels::balancer::v1beta1::MsgCreateBalancerPool;
 use osmosis_std::types::osmosis::gamm::poolmodels::stableswap::v1beta1::{
     MsgCreateStableswapPool, PoolParams as StableSwapPoolParams,
@@ -6,26 +8,18 @@ use osmosis_std::types::osmosis::gamm::poolmodels::stableswap::v1beta1::{
 use osmosis_std::types::osmosis::gamm::v1beta1::{PoolAsset, PoolParams};
 use osmosis_test_tube::{Account, Gamm, Module, Runner, SigningAccount};
 use prop::collection::vec;
-use proptest::prelude::{any_with, prop};
-use proptest::prop_compose;
+use proptest::prelude::*;
 use proptest::strategy::{Just, Strategy};
-use proptest_derive::Arbitrary;
+use proptest::{prop_compose, proptest};
 
 const MAX_SCALE_FACTOR: u64 = 0x7FFF_FFFF_FFFF_FFFF; // 2^63 - 1
 const MAX_POOL_WEIGHT: u64 = 1048575; //2^20 - 1
 
-#[derive(Debug, Clone, PartialEq, Eq, Arbitrary)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum OsmosisPoolType {
     Basic,
-    Balancer {
-        #[proptest(strategy = "vec(1..MAX_POOL_WEIGHT, param_0.len())")]
-        pool_weights: Vec<u64>,
-    },
-    StableSwap {
-        #[proptest(params = "Vec<u64>")]
-        #[proptest(value = "params.clone()")]
-        scaling_factors: Vec<u64>,
-    },
+    Balancer { pool_weights: Vec<u64> },
+    StableSwap { scaling_factors: Vec<u64> },
 }
 
 #[derive(Debug, Clone)]
@@ -34,25 +28,168 @@ pub struct OsmosisTestPool {
     pub pool_type: OsmosisPoolType,
 }
 
+/// Generates a vector of random denoms of the specified size
+pub fn pool_denoms(count: usize) -> impl Strategy<Value = Vec<String>> {
+    Just(Vec::from_iter(0..8))
+        .prop_shuffle()
+        .prop_flat_map(move |x| {
+            let mut denoms = x
+                .iter()
+                .take(count)
+                .map(|i| format!("denom{}", i))
+                .collect::<Vec<String>>();
+            denoms.sort(); // Osmosis requires denoms to be sorted...
+            Just(denoms)
+        })
+}
+
+/// Generates a vector of random denoms with at least one denom in common with
+/// the given denoms.
+pub fn pool_denoms_with_one_common(
+    count: usize,
+    input_denoms: Vec<String>,
+) -> impl Strategy<Value = Vec<String>> {
+    pool_denoms(count).prop_flat_map(move |mut denoms| {
+        if denoms.iter().any(|x| input_denoms.contains(x)) {
+            Just(denoms)
+        } else {
+            let common_denom = input_denoms[0].clone();
+            denoms[0] = common_denom;
+            denoms.sort(); // Osmosis requires denoms to be sorted...
+            Just(denoms)
+        }
+    })
+}
+
+/// Generates a vector of random denoms with one of them being the given
+/// specific_denom
+pub fn pool_denoms_with_one_specific(
+    count: usize,
+    specific_denom: String,
+) -> impl Strategy<Value = Vec<String>> {
+    pool_denoms(count).prop_flat_map(move |mut denoms| {
+        if denoms.iter().any(|x| x == &specific_denom) {
+            Just(denoms)
+        } else {
+            denoms[0] = specific_denom.clone();
+            denoms.sort(); // Osmosis requires denoms to be sorted...
+            Just(denoms)
+        }
+    })
+}
+
+/// Generates a vector of size 2..8 with random u64 values between 1 and u64::MAX
+pub fn pool_liquidity_amounts() -> impl Strategy<Value = Vec<u64>> {
+    vec(1..u64::MAX, 2..8)
+}
+
 prop_compose! {
-    /// Generates a tuple of vectors with (pool_liquidity, scaling_factors) of size 2..8
-    pub fn pool_params()(pool_params in vec((1..u64::MAX, 1..MAX_SCALE_FACTOR), 2..8).prop_filter("scaling factors must be smaller than liquidity",|v| v.iter().all(|(liq, scale)| scale < liq))) -> (Vec<u64>,Vec<u64>) {
-         let (pool_liquidity, scaling_factors): (Vec<u64>,Vec<u64>) = pool_params.into_iter().unzip();
-            (pool_liquidity, scaling_factors)
+    /// Generates a randomly sized vector (size 2-8) of Coins with random amounts and denoms
+    pub fn pool_liquidity()(amounts in pool_liquidity_amounts())(denoms in pool_denoms(amounts.len()), amounts in Just(amounts)) -> Vec<Coin> {
+        amounts.into_iter().zip(denoms).map(|(amount, denom)| Coin { amount: Uint128::from(amount), denom }).collect()
     }
 }
 
 prop_compose! {
-    /// Generates a random OsmosisPoolType with the given scaling factors
-    pub fn pool_type(scaling_factors: Vec<u64>)(pool_type in any_with::<OsmosisPoolType>(scaling_factors)) -> OsmosisPoolType {
-        pool_type
+    /// Generates a randomly sized vector (size 2-8) of Coins with random amounts
+    /// and denoms, where one denom is in common with the given base_liquidity
+    pub fn pool_liquidity_with_one_common_denom(base_liquidity: Vec<Coin>)
+        (amounts in pool_liquidity_amounts())
+        (
+            denoms in pool_denoms_with_one_common(amounts.len(),
+            base_liquidity.iter().map(|x| x.denom.clone()).collect()),
+            amounts in Just(amounts)
+        ) -> Vec<Coin> {
+            amounts.into_iter().zip(denoms).map(|(amount, denom)|
+                Coin { amount: Uint128::from(amount), denom
+            }).collect()
+        }
+}
+
+prop_compose! {
+    /// Generates a randomly sized vector (size 2-8) of Coins with random amounts
+    /// and denoms, where one denom is the given specific_denom
+    pub fn pool_liquidity_with_one_specific_denom(specific_denom: String)
+        (amounts in pool_liquidity_amounts())
+        (
+            denoms in pool_denoms_with_one_specific(amounts.len(), specific_denom.clone()),
+            amounts in Just(amounts)
+        ) -> Vec<Coin> {
+            amounts.into_iter().zip(denoms).map(|(amount, denom)|
+                Coin { amount: Uint128::from(amount), denom
+            }).collect()
+        }
+}
+
+/// Generates scaling factors for an Osmosis StableSwap pool for the given liquidity
+pub fn scaling_factors(pool_liquidity: &Vec<Coin>) -> impl Strategy<Value = Vec<u64>> {
+    pool_liquidity
+        .iter()
+        .map(|x| {
+            let max_scale_factor = if x.amount.u128() > MAX_SCALE_FACTOR.into() {
+                MAX_SCALE_FACTOR
+            } else {
+                x.amount.u128() as u64
+            };
+            1..max_scale_factor
+        })
+        .collect::<Vec<Range<u64>>>()
+}
+
+prop_compose! {
+    /// Generates a tuple of vectors with (pool_liquidity, scaling_factors)
+    pub fn pool_params()(pool_liq in pool_liquidity())(scaling_factors in scaling_factors(&pool_liq), pool_liquidity in Just(pool_liq)) -> (Vec<Coin>,Vec<u64>) {
+        (pool_liquidity, scaling_factors)
+    }
+}
+
+/// Generates a random OsmosisPoolType
+pub fn pool_type(pool_liquidity: &Vec<Coin>) -> impl Strategy<Value = OsmosisPoolType> {
+    prop_oneof![
+        Just(OsmosisPoolType::Basic),
+        vec(1..MAX_POOL_WEIGHT, pool_liquidity.len())
+            .prop_map(|pool_weights| { OsmosisPoolType::Balancer { pool_weights } }),
+        scaling_factors(pool_liquidity)
+            .prop_map(|scaling_factors| { OsmosisPoolType::StableSwap { scaling_factors } }),
+    ]
+}
+
+prop_compose! {
+    /// Generates a random OsmosisTestPool with the given liquidity
+    pub fn test_pool_from_liquidity(pool_liquidity: Vec<Coin>)(pool_type in pool_type(&pool_liquidity), liquidity in Just(pool_liquidity)) -> OsmosisTestPool {
+        OsmosisTestPool {
+            liquidity,
+            pool_type,
+        }
     }
 }
 
 prop_compose! {
     /// Generates a random OsmosisTestPool with 2..8 assets
-    pub fn test_pool()(pool_params in pool_params())(pool_type in pool_type(pool_params.clone().1), pool_liquidity in Just(pool_params.0)) -> OsmosisTestPool {
-        let liquidity = pool_liquidity.iter().enumerate().map(|(i, liq)| Coin::new(*liq as u128, format!("denom{}", i))).collect();
+    pub fn test_pool()(pool_liquidity in pool_liquidity())(
+        test_pool in test_pool_from_liquidity(pool_liquidity)
+    ) -> OsmosisTestPool {
+        test_pool
+    }
+}
+
+prop_compose! {
+    /// Generates a random OsmosisTestPool with one denom in common with the given base pool
+    pub fn reward_pool(base_pool: OsmosisTestPool)(pool_liquidity in pool_liquidity_with_one_common_denom(base_pool.liquidity))(
+        pool_type in pool_type(&pool_liquidity), liquidity in Just(pool_liquidity)
+    ) -> OsmosisTestPool {
+        OsmosisTestPool {
+            liquidity,
+            pool_type,
+        }
+    }
+}
+
+prop_compose! {
+    /// Generates a random OsmosisTestPool with one denom being the given specific denom
+    pub fn pool_with_denom(specific_denom: String)(pool_liquidity in pool_liquidity_with_one_specific_denom(specific_denom))(
+        pool_type in pool_type(&pool_liquidity), liquidity in Just(pool_liquidity)
+    ) -> OsmosisTestPool {
         OsmosisTestPool {
             liquidity,
             pool_type,
@@ -124,5 +261,127 @@ pub fn create_osmosis_pool<'a, R: Runner<'a>>(
             .data
             .pool_id
         }
+    }
+}
+
+proptest! {
+    #![proptest_config(ProptestConfig {
+        cases: 10000,
+        max_local_rejects: 0,
+        ..ProptestConfig::default()
+    })]
+
+    #[test]
+    fn test_pool_params(params in pool_params()) {
+        let (pool_liquidity, scaling_factors): (Vec<Coin>,Vec<u64>) = params;
+        assert_eq!(pool_liquidity.len(), scaling_factors.len());
+        assert!(pool_liquidity.iter().all(|liq| liq.amount.u128() > 0));
+        assert!(scaling_factors.iter().all(|scale| scale > &0));
+        assert!(scaling_factors.iter().all(|scale| scale < &MAX_SCALE_FACTOR));
+        assert!(pool_liquidity.iter().all(|liq| liq.amount.u128() < u64::MAX as u128));
+        assert!(pool_liquidity.iter().zip(scaling_factors.iter()).all(|(liq, scale)| (*scale as u128) < liq.amount.u128()));
+    }
+
+    #[test]
+    fn test_denoms(mut denoms in pool_denoms(8)) {
+        assert!(denoms.len() == 8);
+        denoms.sort();
+        assert_eq!(denoms, Vec::from_iter(0..8).iter().map(|i| format!("denom{}", i)).collect::<Vec<String>>());
+    }
+
+    #[test]
+    fn test_denoms_one_common(denoms in vec(2..8usize,2).prop_flat_map(|counts| pool_denoms(counts[0]).prop_flat_map(move |base_denoms| {
+        (Just(base_denoms.clone()), pool_denoms_with_one_common(counts[1], base_denoms.clone()))
+    }))) {
+        let (base_denoms, other_denoms) = denoms;
+        assert!(base_denoms.iter().any(|denom| other_denoms.contains(denom)));
+        if base_denoms.len() != other_denoms.len() {
+            assert_ne!(base_denoms, other_denoms);
+        }
+    }
+
+    #[test]
+    fn test_denoms_one_specific(denoms in vec(2..8usize,2).prop_flat_map(|counts| pool_denoms(counts[0]).prop_flat_map(move |base_denoms| {
+        (Just(base_denoms.clone()), pool_denoms_with_one_specific(counts[1], base_denoms[0].clone()))
+    }))) {
+        let (base_denoms, other_denoms) = denoms;
+        let common_denom = &base_denoms[0];
+        assert!(other_denoms.contains(common_denom));
+    }
+
+    #[test]
+    fn test_pool_with_denom(pool in pool_with_denom(String::from("requested_denom"))) {
+        let OsmosisTestPool { liquidity, pool_type } = pool;
+        assert!(liquidity.iter().any(|liq| liq.denom == "requested_denom"));
+        assert!(liquidity.len() >= 2);
+        assert!(liquidity.len() <= 8);
+        assert!(liquidity.iter().all(|liq| liq.amount.u128() > 0));
+        assert!(liquidity.iter().all(|liq| liq.amount.u128() < u64::MAX as u128));
+        match pool_type {
+            OsmosisPoolType::Basic => {}
+            OsmosisPoolType::Balancer { pool_weights } => {
+                assert_eq!(pool_weights.len(), liquidity.len());
+                assert!(pool_weights.iter().all(|weight| weight > &0));
+                assert!(pool_weights.iter().all(|weight| weight < &MAX_POOL_WEIGHT));
+            }
+            OsmosisPoolType::StableSwap { scaling_factors } => {
+                assert_eq!(scaling_factors.len(), liquidity.len());
+                assert!(scaling_factors.iter().all(|scale| scale > &0));
+                assert!(scaling_factors.iter().all(|scale| scale < &MAX_SCALE_FACTOR));
+                assert!(liquidity.iter().zip(scaling_factors.iter()).all(|(liq, scale)| (*scale as u128) < liq.amount.u128()));
+            }
+        }
+    }
+
+    #[test]
+    fn test_test_pool(pool in test_pool()) {
+        let OsmosisTestPool { liquidity, pool_type } = pool;
+        assert!(liquidity.len() >= 2);
+        assert!(liquidity.len() <= 8);
+        assert!(liquidity.iter().all(|liq| liq.amount.u128() > 0));
+        assert!(liquidity.iter().all(|liq| liq.amount.u128() < u64::MAX as u128));
+        match pool_type {
+            OsmosisPoolType::Basic => {}
+            OsmosisPoolType::Balancer { pool_weights } => {
+                assert_eq!(pool_weights.len(), liquidity.len());
+                assert!(pool_weights.iter().all(|weight| weight > &0));
+                assert!(pool_weights.iter().all(|weight| weight < &MAX_POOL_WEIGHT));
+            }
+            OsmosisPoolType::StableSwap { scaling_factors } => {
+                assert_eq!(scaling_factors.len(), liquidity.len());
+                assert!(scaling_factors.iter().all(|scale| scale > &0));
+                assert!(scaling_factors.iter().all(|scale| scale < &MAX_SCALE_FACTOR));
+                assert!(liquidity.iter().zip(scaling_factors.iter()).all(|(liq, scale)| (*scale as u128) < liq.amount.u128()));
+            }
+        }
+    }
+
+    #[test]
+    fn test_reward_pool((pool, reward_pool) in test_pool().prop_flat_map(|base_pool| (Just(base_pool.clone()), reward_pool(base_pool)))) {
+        let OsmosisTestPool { liquidity: reward_liquidity, pool_type: reward_pool_type } = reward_pool;
+        assert!(reward_liquidity.len() >= 2);
+        assert!(reward_liquidity.len() <= 8);
+        assert!(reward_liquidity.iter().all(|liq| liq.amount.u128() > 0));
+        assert!(reward_liquidity.iter().all(|liq| liq.amount.u128() < u64::MAX as u128));
+        match reward_pool_type {
+            OsmosisPoolType::Basic => {}
+            OsmosisPoolType::Balancer { pool_weights } => {
+                assert_eq!(pool_weights.len(), reward_liquidity.len());
+                assert!(pool_weights.iter().all(|weight| weight > &0));
+                assert!(pool_weights.iter().all(|weight| weight < &MAX_POOL_WEIGHT));
+            }
+            OsmosisPoolType::StableSwap { scaling_factors } => {
+                assert_eq!(scaling_factors.len(), reward_liquidity.len());
+                assert!(scaling_factors.iter().all(|scale| scale > &0));
+                assert!(scaling_factors.iter().all(|scale| scale < &MAX_SCALE_FACTOR));
+                assert!(reward_liquidity.iter().zip(scaling_factors.iter()).all(|(liq, scale)| (*scale as u128) < liq.amount.u128()));
+            }
+        }
+
+        let OsmosisTestPool {liquidity, pool_type: _ } = pool;
+        let reward_denoms = reward_liquidity.iter().map(|liq| liq.denom.clone()).collect::<Vec<String>>();
+        let base_denoms = liquidity.iter().map(|liq| liq.denom.clone()).collect::<Vec<String>>();
+        // Assert that reward denoms has at least one denom in common with base denoms
+        assert!(reward_denoms.iter().any(|denom| base_denoms.contains(denom)));
     }
 }
