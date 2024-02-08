d73cedffc22c573e75a23192cf426754bae9f6fb
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2024-02-08 19:02:49 +0100

    feat: Add astroport incentives contract

--- src/astroport/utils.rs
+++ src/astroport/utils.rs
@@ -20,12 +20,13 @@ use astroport::vesting::{
     Cw20HookMsg as VestingHookMsg, InstantiateMsg as VestingInstantiateMsg, VestingAccount,
     VestingSchedule, VestingSchedulePoint,
 };
+use astroport_v3::incentives::InstantiateMsg as IncentivesInstantiateMsg;
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{to_json_binary, Addr, Binary, Coin, Event, Uint128, Uint64};
 use cw20::{BalanceResponse, Cw20Coin, Cw20ExecuteMsg, Cw20QueryMsg, MinterResponse};
 use test_tube::{Account, Module, Runner, SigningAccount, Wasm};
 
-pub const ASTROPORT_CONTRACT_NAMES: [&str; 13] = [
+pub const ASTROPORT_CONTRACT_NAMES: [&str; 14] = [
     "astroport_token",
     "astroport_native_coin_registry",
     "astroport_factory",
@@ -39,6 +40,7 @@ pub const ASTROPORT_CONTRACT_NAMES: [&str; 13] = [
     "astroport_whitelist",
     "astroport_liquidity_manager",
     "astroport_pair_concentrated",
+    "astroport_incentives",
 ];
 
 #[cw_serde]
@@ -67,6 +69,7 @@ pub struct AstroportContracts {
     pub vesting: Contract,
     pub whitelist: Contract,
     pub liquidity_manager: Contract,
+    pub incentives: Contract,
 }
 
 impl AstroportContracts {
@@ -264,6 +267,30 @@ where
         .data
         .address;
 
+    // Instantiate incentives contract
+    println!("Instantiating incentives contract ...");
+    let incentives = wasm
+        .instantiate(
+            code_ids["astroport_incentives"],
+            &IncentivesInstantiateMsg {
+                astro_token: astroport_v3::asset::AssetInfo::Token {
+                    contract_addr: Addr::unchecked(&astro_token),
+                },
+                factory: factory.clone(),
+                guardian: None,
+                incentivization_fee_info: None,
+                owner: admin.address(),
+                vesting_contract: vesting.clone(),
+            },
+            Some(&admin.address()),       // contract admin used for migration
+            Some("Astroport Incentives"), // contract label
+            &[],                          // funds
+            admin,                        // signer
+        )
+        .unwrap()
+        .data
+        .address;
+
     // Update factory config to add generator
     println!("Updating factory config to add generator ...");
     let _res = wasm
@@ -388,6 +415,7 @@ where
             liquidity_manager,
             code_ids["astroport_liquidity_manager"],
         ),
+        incentives: Contract::new(incentives, code_ids["astroport_incentives"]),
     }
 }
 
@@ -641,7 +669,7 @@ pub fn get_astroport_multitest_contracts() -> HashMap<String, ContractType> {
         "astroport_pair"
     ));
 
-    // Liquidity manager and concentrated pair don't have query entrypoint in contract module
+    // Liquidity manager, incentives, and concentrated pair don't have query entrypoint in contract module
     contract_wrappers.extend(vec![
         (
             "astroport_liquidity_manager".to_string(),
@@ -663,6 +691,17 @@ pub fn get_astroport_multitest_contracts() -> HashMap<String, ContractType> {
                     astroport_pair_concentrated::queries::query,
                 )
                 .with_reply(astroport_pair_concentrated::contract::reply),
+            ) as Box<dyn apollo_cw_multi_test::Contract<Empty>>,
+        ),
+        (
+            "astroport_incentives".to_string(),
+            Box::new(
+                ContractWrapper::new_with_empty(
+                    astroport_incentives::execute::execute,
+                    astroport_incentives::instantiate::instantiate,
+                    astroport_incentives::query::query,
+                )
+                .with_reply(astroport_incentives::reply::reply),
             ) as Box<dyn apollo_cw_multi_test::Contract<Empty>>,
         ),
     ]);
